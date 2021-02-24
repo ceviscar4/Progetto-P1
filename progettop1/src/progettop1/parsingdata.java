58144388e4e284e79d3abef6bc6d5d0e3bf04e03
Francesco Cerrone <france.cerrone@outlook.it>
2021-02-24 18:05:17 +0100

    parsingdata

---
+++ progettop1/src/progettop1/parsingdata.java
@@ -0,0 +1,30 @@
+package progettop1;
+
+public class parsingdata {
+	//parsingdata sarà la superclasse grazie alla quale i parser otterranno i dati di openweather
+	
+	
+	public class Parsing{
+		
+		//qui è presente la stringa che prenderà la key della api di openweather
+		
+		private String Apikey;
+		
+		//costruttore della della classe Parsing
+		
+		public Parsing(String Apikey) {
+			this.Apikey=Apikey;
+		}
+
+		
+		//getters and setters generati per  dare e settare Apikey
+		public String getApikey() {
+			return Apikey;
+		}
+
+		public void setApikey(String apikey) {
+			Apikey = apikey;
+		}
+	}
+
+}
